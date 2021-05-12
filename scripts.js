e3ddae53acc23db2c327386b0a1fc447387cd77d
Alexandru <76220253+AlexandruLuca20@users.noreply.github.com>
2021-05-12 14:17:50 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+var radius = prompt("Please enter Radius: ");
+
+var height = prompt("Please enter Height: ");
+
+const Pi = 3.14;
+
+var volume = parseInt(radius) * parseInt(radius) * 
+parseInt(height) * Pi;
+
+alert("Cylinder volume is: " + volume)
